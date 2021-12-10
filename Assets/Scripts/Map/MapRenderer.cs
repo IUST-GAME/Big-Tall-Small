522af84b9603ed7b253d3c38cb3e5897980cd177
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-12-10 16:11:32 +0330

    feat: add level 001 map

--- Assets/Scripts/Map/MapRenderer.cs
+++ Assets/Scripts/Map/MapRenderer.cs
@@ -14,24 +14,55 @@ namespace Map
         private const int VerticalBoxes = 18;
         private const int HorizontalBoxes = 24;
 
-        public GameObject BlueTestBlocks;
+        public GameObject Block;
+        public GameObject Background1;
+        public GameObject Background2;
+
+
+        void Start()
+        {
+            RenderMap("level_001");
+        }
 
         public void RenderMap(string mapName)
         {
-            var path = Application.dataPath + "/Maps/level_001.json";
+            var mapData = ReadJsonFile(mapName);
+
+
+            Debug.Log("going to instantiate blocks");
+            for (var i = 0; i < 24; i++)
+            {
+                for (var j = 0; j < 18; j++)
+                {
+                    GameObject go;
+                    switch (mapData.Rows[j].Blocks[i])
+                    {
+                        // It is an empty cell
+                        case 0:
+                            go = Instantiate((i + j) %2 == 0 ? Background1 : Background2);
+                            break;
+                        // It is a wall cell
+                        case 1:
+                            go = Instantiate(Block);
+                            break;
+                        default:
+                            continue;
+                    }
+                    
+                    // For now I just hardcode the numbers
+                    // Numbers -6.381f, 4.722f represents the top left point of screen
+                    // (10/VerticalBoxes) = 0.555 and ((10) * Ratio)/HorizontalBoxes
+                    go.transform.position =
+                        new Vector3(-6.381f + i * 0.555f, 4.722f - j * 0.5555f, transform.position.z);
+                }
+            }
+        }
+
+        private Map ReadJsonFile(string mapName)
+        {
+            var path = Application.dataPath + "/Maps/" + mapName + ".json";
             var jsonString = File.ReadAllText(path);
-            var mapData = JsonUtility.FromJson<Map>(jsonString);
-
-
-            var g = Instantiate(
-                BlueTestBlocks,
-                new Vector3(
-                    this.gameObject.transform.position.x,
-                    this.gameObject.transform.position.y,
-                    this.gameObject.transform.position.z
-                ),
-                Quaternion.identity,
-                this.gameObject.transform);
+            return JsonUtility.FromJson<Map>(jsonString);
         }
     }
 }
