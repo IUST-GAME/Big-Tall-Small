5d2f012b67767b3eb4c3027e3ec2d02b5fc15485
Ramtin Ehsani <ramtin79e@gmail.com>
2022-01-25 14:14:36 +0330

    Add: flip sync with photon

--- Assets/Scripts/Models/Players/CharacterController2D.cs
+++ Assets/Scripts/Models/Players/CharacterController2D.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 using UnityEngine.Events;
 
-public class CharacterController2D : MonoBehaviour
+public class CharacterController2D : MonoBehaviour, IPunObservable
 {
+	private PhotonView m_view;
 	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
 	// [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
@@ -34,7 +36,16 @@ public class CharacterController2D : MonoBehaviour
 
 		if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
+		m_view = GetComponent<PhotonView>();
+		AddObservable();
 		
+	}
+	private void AddObservable()
+	{
+		if (!m_view.ObservedComponents.Contains(this))
+		{
+			m_view.ObservedComponents.Add(this);
+		}
 	}
 
 	private void FixedUpdate()
@@ -99,5 +110,17 @@ public class CharacterController2D : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.IsWriting)
+		{
+			stream.SendNext(transform.localScale);
+		}
+		else
+		{
+			transform.localScale = (Vector3) stream.ReceiveNext();
+		}
 	}
 }
